0875310f5d340f5ffbc9665cae6d6f30638e8bea
Joshua Carp <jm.carp@gmail.com>
2016-09-13 22:24:46 -0400

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.2)

---
+++ index.js
@@ -0,0 +1,36 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      
+      app: {
+        title: 'Google Analytics',
+        description: 'google analytics plugin',
+        main: 'plugins/google_analytics/app'
+      },
+      
+      
+      hacks: [
+        'plugins/google_analytics/hack'
+      ]
+      
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+    
+
+  });
+};
