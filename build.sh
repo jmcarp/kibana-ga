d16610db6c971524c206e589b649d92055ae2c82
Joshua Carp <jm.carp@gmail.com>
2016-09-14 23:38:49 -0400

    Add build script.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf build
+mkdir -p build/ga
+
+include=("package.json" "index.js" "public")
+for path in $include; do
+  cp $path build/ga
+done
+
+pushd build/ga
+  npm install --production
+popd
+
+tar cvf build/release.tar.gz -C build ga
